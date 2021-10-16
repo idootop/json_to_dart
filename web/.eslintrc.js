715217cea98195b499c6ffe5df7ac0ab097a7e34
WJG <wjg12341234@163.com>
2021-10-16 11:07:26 +0800

    init eslint styelint

---
+++ web/.eslintrc.js
@@ -0,0 +1,65 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+  },
+  globals: {
+    JSX: 'readonly',
+  },
+  extends: [
+    'alloy',
+    'alloy/react',
+    'alloy/typescript',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'prettier',
+  ],
+  plugins: ['import', 'simple-import-sort'],
+  rules: {
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'no-unused-vars': 'off',
+    'guard-for-in': 'off',
+    'react/prop-types': 'warn',
+    'react/style-prop-object': 'warn',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'simple-import-sort/imports': 'warn',
+    'simple-import-sort/exports': 'warn',
+    'import/first': 'warn',
+    'import/newline-after-import': 'warn',
+    'import/no-duplicates': 'warn',
+    '@typescript-eslint/explicit-function-return-type': [
+      'error',
+      {
+        allowExpressions: true,
+        allowTypedFunctionExpressions: true,
+        allowHigherOrderFunctions: true,
+        allowDirectConstAssertionInArrowFunctions: true,
+        allowConciseArrowFunctionExpressionsStartingWithVoid: true,
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['*.tsx'],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': off,
+      },
+    },
+  ],
+};
